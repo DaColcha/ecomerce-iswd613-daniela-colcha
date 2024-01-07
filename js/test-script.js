1587fc234f972bf40bd63c9d9d8bf7ba4e4ca0aa
Alejandra Colcha <da.colcha@hotmail.com>
2024-01-06 23:43:19 -0500

    Práctica 11 060123

--- js/test-script.js
+++ js/test-script.js
@@ -4,3 +4,12 @@ function suma(a,b){
 }
 
 console.log('Hola mundo '+ suma(2,3));
+
+function initializePage(){
+    console.log('******Onload complete*****');
+    console.log(navigator.userAgent);
+    console.log(navigator.language);
+    //Tarea, implementar la funcionalidad de copiado
+    //crear un input con texto predefinido con un botón de copiar 
+    console.log(navigator.ap);
+}
